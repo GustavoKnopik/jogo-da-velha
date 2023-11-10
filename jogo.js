3ff104e8afdc50636255664ffd1c205f046d5a84
Gustavo Knopik <106327366+GustavoKnopik@users.noreply.github.com>
2023-11-10 09:17:25 -0300

    sa

--- jogo.js
+++ jogo.js
@@ -8,8 +8,20 @@ const JOGADOR_O = "O";
 
 let jogoAcabou = false;
 
-document.addEventListener("click", (event) =>) {
- if (event.target.matches(".celula")&& !jogoAcabou) {
-jogar (event.target.id);
-}
+document.addEventListener("click", (event) => {
+    if (event.target.matches(".celula") && !jogoAcabou) {
+        jogar(event.target.id);
+    }
 });
+function jogar(id) {
+    const celula = document.getElementById(id);
+    if (celula.textContent === "") {
+        turno = checarTurno ? JOGADOR_X : JOGADOR_O;
+  checarTurno = !checarTurno;
+   celula.textContent = turno;
+   celula.classList.add(turno);
+   celula.removeEventListener("click", jogar);
+   verificarVencedor(turno);
+
+    }
+}
